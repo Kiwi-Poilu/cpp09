8c619a81c46a2d62a4734afe92b8d66e57445997
Sofiane BOUATTOUR <sobouatt@e1r6p3.clusters.42paris.fr>
2023-05-12 22:51:59 +0200

    yeaah

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,11 +2,11 @@
 
 void	performRPN(char *str)
 {
-	int i = -1;
+	int i = 0;
 	int a, b, tmp;
 	std::stack<int> rpn;
 
-	while (str[++i])
+	while (str[i] != '\0')
 	{
 		if (isdigit(str[i]))
 			rpn.push(atoi(str + i));
@@ -26,13 +26,23 @@ void	performRPN(char *str)
 		}
 		else
 		{
-			std::cout << "1Error: invalid token in the expression" << std::endl;
+			std::cout << "Error: invalid token in the expression" << std::endl;
 			return ;
 		}
-		if (str[++i] && str[i] != ' ')
+		i++;
+		if (str[i])
 		{
-			std::cout << "2Error: invalid token in the expression" << std::endl;
-			return ;
+			if (str[i] != ' ')
+			{
+				std::cout << "Error: invalid token in the expression" << std::endl;
+				return ;
+			}
+			i++;
+			if (!str[i])
+			{
+				std::cout << "Error: invalid token in the expression" << std::endl;
+				return ;
+			}
 		}
 	}
 	if (rpn.size() == 1)
@@ -46,48 +56,55 @@ int	main(int ac, char **av)
 {
 	// int a, b;
 	// int tmp;
-	// std::stack<int> rpn;
-	// std::string str;
+	// // std::stack<int> rpn;
+	// // std::string str;
 	if (ac != 2)
 	{
 		std::cout << "Error: invalid number of arguments" << std::endl;
 		return (0);
 	}
-	// int i = -1;	
+	// int i = 0;	
 	// str = av[1];
-	// while (str[++i])
+	// while (str[i])
 	// {
-	// 	if (isdigit(str[i]))
-	// 		rpn.push(atoi(str.c_str() + i));
-	// 	else if (isOperator(str[i]))
-	// 	{
-	// 		if (rpn.size() < 2)
-	// 		{
-	// 			std::cout << "Error: not enough operands to apply an operator" << std::endl;
-	// 			return (0);
-	// 		}
-	// 		a = rpn.top();
-	// 		rpn.pop();
-	// 		b = rpn.top();
-	// 		rpn.pop();
-	// 		tmp = calculate(str[i], a, b);
-	// 		rpn.push(tmp);
-	// 	}
-	// 	else
-	// 	{
-	// 		std::cout << "Error: invalid token in the expression" << std::endl;
-	// 		return (0);
-	// 	}
-	// 	if (str[++i] && str[i] != ' ')
-	// 	{
-	// 		std::cout << "Error: invalid token in the expression" << std::endl;
-	// 		return (0);
-	// 	}
+		// if (isdigit(str[i]))
+			// rpn.push(atoi(str.c_str() + i));
+		// else if (isOperator(str[i]))
+		// {
+			// if (rpn.size() < 2)
+			// {
+				// std::cout << "Error: not enough operands to apply an operator" << std::endl;
+				// return (0);
+			// }
+			// a = rpn.top();
+			// rpn.pop();
+			// b = rpn.top();
+			// rpn.pop();
+			// tmp = calculate(str[i], a, b);
+			// rpn.push(tmp);
+		// }
+		// else
+		// {
+			// std::cout << "Error: invalid token in the expression" << std::endl;
+			// return (0);
+		// }
+		// i++;
+		// if (str[i])
+		// {
+			// if (str[i] == ' ')
+				// i++;
+			// else
+			// {
+				// std::cout << "Error: invalid token in the expression" << std::endl;
+				// return (0);
+			// }
+		// }
+// 
 	// }
 	// if (rpn.size() == 1)
-	// 	std::cout << rpn.top() << std::endl;
+		// std::cout << rpn.top() << std::endl;
 	// else
-	// 	std::cout << "Error: not a valid rpn expression" << std::endl;
+		// std::cout << "Error: not a valid rpn expression" << std::endl;
 	performRPN(av[1]);
 	return (0);
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,13 +2,130 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <deque>
+#include <sys/time.h>
+
+void	mergeDeque(std::deque<int> &array, int left, int mid, int right)
+{
+	int subArrayOne = mid - left + 1;
+	int subArrayTwo = right - mid;
+
+	std::deque<int> leftArray(subArrayOne);
+	std::deque<int> rightArray(subArrayTwo);
+	for (int i = 0; i < subArrayOne; i++)
+		leftArray[i] = array[left + i];
+	for (int j = 0; j < subArrayTwo; j++)
+		rightArray[j] = array[mid + 1 + j];
+	int indexOfSubArrayOne = 0;
+	int indexOfSubArrayTwo = 0;
+	int indexOfMergedArray = left;
+
+	while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+	{
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+		{
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+		{
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne)
+	{
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo)
+	{
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void	mergeSortDeque(std::deque<int> &array, int begin, int end)
+{
+	if (begin >= end)
+		return;
+    int mid = begin + (end - begin) / 2;
+    mergeSortDeque(array, begin, mid);
+    mergeSortDeque(array, mid + 1, end);
+    mergeDeque(array, begin, mid, end);
+}
+
+void	mergeVec(std::vector<int> &array, int left, int mid, int right)
+{
+	int subArrayOne = mid - left + 1;
+	int subArrayTwo = right - mid;
+
+	std::vector<int> leftArray(subArrayOne);
+	std::vector<int> rightArray(subArrayTwo);
+	for (int i = 0; i < subArrayOne; i++)
+		leftArray[i] = array[left + i];
+	for (int j = 0; j < subArrayTwo; j++)
+		rightArray[j] = array[mid + 1 + j];
+	int indexOfSubArrayOne = 0;
+	int indexOfSubArrayTwo = 0;
+	int indexOfMergedArray = left;
+
+	while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+	{
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+		{
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+		{
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne)
+	{
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo)
+	{
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void	mergeSortVec(std::vector<int> &array, int begin, int end)
+{
+	if (begin >= end)
+		return;
+    int mid = begin + (end - begin) / 2;
+    mergeSortVec(array, begin, mid);
+    mergeSortVec(array, mid + 1, end);
+    mergeVec(array, begin, mid, end);
+}
 
 int	main(int ac, char **av)
 {
 	int i = 1;
 	int tmp;
-	std::vector<int> test; 
-	if (ac < 2)
+	std::vector<int> arrayVec; 
+	std::deque<int> arrayDeque;
+	if (ac < 2) 
 	{
 		std::cout << "Error: not enough arguments" << std::endl;
 		return (0);
@@ -22,7 +139,32 @@ int	main(int ac, char **av)
 			std::cout << "Error: not a valid list of integers" << std::endl;
 			return (0);
 		}
-		std::cout << "[" << i << "]" << tmp << "," << std::endl;
+		arrayVec.push_back(tmp);
+		arrayDeque.push_back(tmp);
 		i++;
 	}
+	struct timeval  tv1, tv2;
+	std::cout << "Before sorting with vector:";
+	for (int i = 0; i < arrayVec.size();i++)
+		std::cout << " " << arrayVec[i];
+	std::cout << std::endl;
+	gettimeofday(&tv1, NULL);
+	mergeSortVec(arrayVec, 0, arrayVec.size() - 1);
+	gettimeofday(&tv2, NULL);
+	std::cout << "After Vector:";
+	for (int i = 0; i < arrayVec.size();i++)
+		std::cout << " " << arrayVec[i];
+	printf("\nTime taken in execution = %f seconds\n",
+     	(double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+     	(double) (tv2.tv_sec - tv1.tv_sec));
+	
+	std::cout << "\nBefore sorting with deque:";
+	for (int i = 0; i < arrayDeque.size();i++)
+		std::cout << " " << arrayDeque[i];
+	std::cout << std::endl;
+	mergeSortDeque(arrayDeque, 0, arrayDeque.size() - 1);
+	std::cout << "\nAfter Deque:";
+	for (int i = 0; i < arrayDeque.size();i++)
+		std::cout << " " << arrayDeque[i];
+	return (0);
 }
